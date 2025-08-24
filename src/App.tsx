d15ad9c69459fd953683dcbc05fe63970da12333
Dmytro Kyrychyk <maestrokd.reg@gmail.com>
2025-08-24 14:57:16 -0700

    feat: GH-4 - Create Multiplication Jumping Rabbit game

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router";
 import MultiplicationTrainer from "./pages/MultiplicationTrainer.tsx";
 import Menu from "./pages/Menu.tsx";
+import RabbitJumpX9 from "./pages/JumpingRabbit.tsx";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
                 <Route path="/" element={<Menu/>}/>
                 <Route path="/about" element={<h1 className="text-2xl">About Page</h1>}/>
                 <Route path="/game1" element={<MultiplicationTrainer/>}/>
+                <Route path="/game2" element={<RabbitJumpX9/>}/>
             </Routes>
         </>
     );
